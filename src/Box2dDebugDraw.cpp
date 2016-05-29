16bedb6c98580f02a995de370476aee58b8821b8
xxAtrain223 <anthony.l.stewart@outlook.com>
2016-05-29 04:40:35 -0400

    Fixed Box2dDebugDraw drawing an extra pixel for polygon outlines.

--- src/Box2dDebugDraw.cpp
+++ src/Box2dDebugDraw.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "Box2dDebugDraw.hpp"
+#include "Print.hpp"
+#include <Thor/Vectors/VectorAlgebra2D.hpp>
 
 using namespace std;
 
@@ -14,8 +16,21 @@ void Box2dDebugDraw::DrawPolygon(const b2Vec2 *vertices, int32 vertexCount, cons
     s.setOutlineColor(B2SFColor(color));
     s.setOutlineThickness(1.f);
 
+    vector<sf::Vector2f> points;
+    sf::Vector2f center;
     for (int i = 0; i < vertexCount; i++)
-        s.setPoint(i, sf::Vector2f(vertices[i].x * PixelsPerMeter, vertices[i].y * -PixelsPerMeter));
+    {
+        points.emplace_back((sf::Vector2f(vertices[i].x * PixelsPerMeter, vertices[i].y * -PixelsPerMeter)));
+        center += points.back();
+    }
+    center /= (float)points.size();
+
+    for (int i = 0; i < vertexCount; i++)
+    {
+        sf::Vector2f p = points[i] - center;
+        thor::setLength(p, thor::length(p) - 1.4f);
+        s.setPoint(i, p + center);
+    }
 
     Window->draw(s);
 }
@@ -29,8 +44,21 @@ void Box2dDebugDraw::DrawSolidPolygon(const b2Vec2 *vertices, int32 vertexCount,
     s.setOutlineColor(B2SFColor(color));
     s.setOutlineThickness(1.f);
 
+    vector<sf::Vector2f> points;
+    sf::Vector2f center;
     for (int i = 0; i < vertexCount; i++)
-        s.setPoint(i, sf::Vector2f(vertices[i].x * PixelsPerMeter, vertices[i].y * -PixelsPerMeter));
+    {
+        points.emplace_back((sf::Vector2f(vertices[i].x * PixelsPerMeter, vertices[i].y * -PixelsPerMeter)));
+        center += points.back();
+    }
+    center /= (float)points.size();
+
+    for (int i = 0; i < vertexCount; i++)
+    {
+        sf::Vector2f p = points[i] - center;
+        thor::setLength(p, thor::length(p) - 1.4f);
+        s.setPoint(i, p + center);
+    }
 
     Window->draw(s);
 }
@@ -40,7 +68,7 @@ void Box2dDebugDraw::DrawCircle(const b2Vec2 &center, float32 radius, const b2Co
     sf::CircleShape s;
 
     s.setPosition(center.x * PixelsPerMeter, center.y * -PixelsPerMeter);
-    s.setRadius(radius * PixelsPerMeter);
+    s.setRadius(radius * PixelsPerMeter - 1);
     s.setOutlineThickness(1.f);
     s.setOutlineColor(B2SFColor(color));
 
@@ -52,7 +80,7 @@ void Box2dDebugDraw::DrawSolidCircle(const b2Vec2 &center, float32 radius, const
     sf::CircleShape s;
 
     s.setPosition(center.x * PixelsPerMeter, center.y * -PixelsPerMeter);
-    s.setRadius(radius * PixelsPerMeter);
+    s.setRadius(radius * PixelsPerMeter - 1);
     s.setFillColor(B2SFColor(color, 50));
     s.setOutlineColor(B2SFColor(color));
     s.setOutlineThickness(1.f);
